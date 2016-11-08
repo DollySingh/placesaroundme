9ff11dc6bca1323e08bd7791a1e63eaef137727f
Student <Student@apples-MacBook-Air-3.local>
2016-11-08 10:44:12 +0530

    Initial Commit

---
+++ ColectionPlacesaroundExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColectionPlacesaroundExample
+//
+//  Created by Student on 08/11/16.
+//  Copyright (c) 2016 Abhijit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
